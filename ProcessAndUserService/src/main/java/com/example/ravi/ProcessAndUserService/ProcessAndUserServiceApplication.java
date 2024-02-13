9ee7e5a999a08efb193e0fa39e4e89150c606570
ravishankar2463 <ravishankar2463@gmail.com>
2024-02-14 00:12:56 +0530

    Added Config Server Support

---
+++ ProcessAndUserService/src/main/java/com/example/ravi/ProcessAndUserService/ProcessAndUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ravi.ProcessAndUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessAndUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessAndUserServiceApplication.class, args);
+	}
+
+}
